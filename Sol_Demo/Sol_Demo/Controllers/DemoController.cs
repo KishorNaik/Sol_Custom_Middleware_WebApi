8aa6f8d08bbae22b2b383629e502288a3b03e11e
Kishor Naik <kishor.naik011.net@gmail.com>
2021-04-29 20:22:06 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Controllers/DemoController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sol_Demo.Controllers
+{
+    [Route("api/demo")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        private readonly IMemoryCache memoryCache = null;
+
+        public DemoController(IMemoryCache memoryCache)
+        {
+            this.memoryCache = memoryCache;
+        }
+
+        [HttpGet("getData")]
+        public IActionResult GetApiCacheData()
+        {
+            return base.Ok(this.memoryCache.Get<String>("ResponseCacheKey"));
+        }
+    }
+}
---
+++ Sol_Demo/Sol_Demo/Middleware/ApiMemoryCacheMiddlewareExtension.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Sol_Demo.Middleware
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class ApiMemoryCacheMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ApiMemoryCacheMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+            var memoryCacheService = httpContext.RequestServices.GetRequiredService<IMemoryCache>();
+
+            if (!memoryCacheService.TryGetValue("ResponseCacheKey", out string data))
+            {
+                data = GetApiResponse();
+
+                var cahceEntryOption = new MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromSeconds(10));
+
+                memoryCacheService.Set<String>("ResponseCacheKey", data, cahceEntryOption);
+            }
+
+            return _next(httpContext);
+        }
+
+        private String GetApiResponse()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
+                //HTTP GET
+                var responseTask = client.GetAsync("todos/1").Result;
+                var data = responseTask.Content?.ReadAsStringAsync()?.Result;
+
+                return data;
+            }
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class ApiMemoryCacheMiddlewareExtensionExtensions
+    {
+        public static IApplicationBuilder UseApiMemoryCache(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ApiMemoryCacheMiddleware>();
+        }
+    }
+}
